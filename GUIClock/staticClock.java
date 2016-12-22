0f5b8b7fc22ce70e23c62a4c47e4c0f47870cc76
RahulReddyNannuru <rnannuru@gmail.com>
2016-12-22 18:33:38 -0500

    added beach background

--- GUIClock/staticClock.java
+++ GUIClock/staticClock.java
@@ -1,5 +1,31 @@
 package GUIClock;
 
-public class staticClock {
+import processing.core.PApplet;
+import processing.core.PImage;
+
+public class staticClock extends PApplet{
+	
+	PImage backgroundIMG;
+	String URL = "https://affordableholidaysblog.files.wordpress.com/2015/05/matira-beach-bora-bora2.jpg";
+	
+	@Override
+	public void setup() {
+	
+		size(800,600);
+		backgroundIMG = loadImage(URL,"jpg");
+		backgroundIMG.resize(0, height);
+		image(backgroundIMG,0,0);
+		
+	}
+	
+
+	@Override
+	public void draw() {
+	
+		
+	}
+
+	
+	
 
 }
